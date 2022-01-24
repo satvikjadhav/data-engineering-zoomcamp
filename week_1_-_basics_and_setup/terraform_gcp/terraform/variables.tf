c45c3f56eac9e97bc0e11f40d948bf7441e5e865
Satvik Jadhav <satvikj5@gmail.com>
2022-01-25 00:05:47 +0530

    adding terraform and google cloud notes, and terraform variables and main file

--- week_1_-_basics_and_setup/terraform_gcp/terraform/variables.tf
+++ week_1_-_basics_and_setup/terraform_gcp/terraform/variables.tf
@@ -1,5 +1,36 @@
+locals {
+  data_lake_bucket = "dtc_data_lake"
+}
+
 variable "project" {
   description = "data-engineering-339113"
-  default = "ny-rides-satvik"
-  type = string
+  default     = "data-engineering-339113"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default     = "asia-south1"
+  type        = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default     = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type        = string
+  default     = "trips_data_all"
+}
+
+variable "TABLE_NAME" {
+  description = "BigQuery Table"
+  type        = string
+  default     = "ny_trips"
+}
+
+variable "credentials" {
+  description = "credentials for google cloud account"
 }
