6cfef295df3aede03f89bb242085a80501b0d09c
satvikjadhav <satvikj5@gmail.com>
2022-01-31 18:42:36 +0000

    adding dags local and updating gitignore file

---
+++ week_2_data_ingestion/airflow/dags_local/data_ingestion_local.py
@@ -0,0 +1,45 @@
+import os
+import logging
+from datetime import datetime
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+# Importing our Operators
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+# Using this lib to for converting our csv files to parquet format
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+
+#Paths
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+
+local_workflow = DAG(
+    "LocalIngestionDag",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2021, 1, 1)
+)
+
+url = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv'
+
+URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data'
+URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}'
+OUTPUT_FILE_TEMPLATE = path_to_local_home + '/output_{{execution_date.strftime(\'%Y-%m\')}}.csv'
+
+with local_workflow:
+
+
+    wget_task = BashOperator(
+        task_id = 'wget',
+        bash_command = f'curl -sSL {url} > {OUTPUT_FILE_TEMPLATE}'
+        # bash_command = 'echo "{{execution_date.strftime(\'%Y-%m\')}}"'
+    )
+
+    ingest_task = BashOperator(
+        task_id = 'ingest',
+        bash_command = f'ls {path_to_local_home}'
+    )
+
+    wget_task >> ingest_task
